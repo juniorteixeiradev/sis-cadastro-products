c8e3395eea98db782a237b867051a1a89d75509a
teixeirajrwd <teixeirajrwd@gmail.com>
2023-11-17 00:24:02 -0300

    Update config.ts

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -2,9 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    apiKey: "AIzaSyDoVVbmuZjBQxp39jHexKkk5qm4dMOYSJw",
+    authDomain: "crud-next-1efc9.firebaseapp.com",
+    projectId: "crud-next-1efc9",
+    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
